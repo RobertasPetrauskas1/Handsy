3afeffb90caaecafb6c238ddd97cc7fe78703cf4
Robertas Petrauskas <robertas.petrauskas@ktu.edu>
2021-09-21 15:35:51 +0300

    remove port from env

--- handsy/api/__init__.py
+++ handsy/api/__init__.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-import os
 from handsy.api.endpoints.user import router as user_router
 from handsy.api.endpoints.auth import router as auth_router
 from handsy.api.endpoints.group import router as group_router
 from handsy.api.endpoints.item import router as item_router
 from handsy.api.endpoints.comment import router as comment_router
-
-# load env variables
-port = os.environ.get("PORT")
 
 # init FastAPI and include routes
 app = FastAPI()
